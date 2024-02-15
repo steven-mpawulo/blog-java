6fbd11426a2ec81b140118935b61787d1632db28
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-15 14:34:06 +0300

    works on saving article to database

--- src/main/java/com/example/blog/controllers/ArticleController.java
+++ src/main/java/com/example/blog/controllers/ArticleController.java
@@ -6,10 +6,7 @@ import com.example.blog.repositories.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,5 +31,10 @@ public class ArticleController {
         } else {
             throw new NoArticleFoundException(HttpStatus.NOT_FOUND, "no article found");
         }
+    }
+
+    @PostMapping("/articles")
+    public Article addArticle(@RequestBody Article article) {
+        return articleRepository.save(article);
     }
 }
