7e34ab3798dffe21d5a09c1abe410d9de55cb524
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-19 12:13:42 +0300

    works on delete author controller

--- src/main/java/com/example/blog/controllers/AuthorController.java
+++ src/main/java/com/example/blog/controllers/AuthorController.java
@@ -1,6 +1,7 @@
 package com.example.blog.controllers;
 
 import com.example.blog.exception.NoArticleFoundException;
+import com.example.blog.exception.NoAuthorFoundException;
 import com.example.blog.models.Author;
 import com.example.blog.repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,17 @@ public class AuthorController {
     @PostMapping("/authors")
     public Author addAuthor(@RequestBody Author author) {
         return authorRepository.save(author);
+    }
+
+    @DeleteMapping("/authors/{authorId}")
+    public void deleteAuthor(@PathVariable Long authorId) {
+        Optional<Author> author = authorRepository.findById(authorId);
+        if (author.isPresent()) {
+            authorRepository.deleteById(authorId);
+        } else {
+            throw new NoAuthorFoundException(HttpStatus.NOT_FOUND, "author already deleted");
+        }
+        authorRepository.deleteById(authorId);
     }
 
 }
